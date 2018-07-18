ddeab51bd8fc09a94fa8c4f41376684530f7583a
lijin <coolagain2006@gmail.com>
2018-07-18 10:46:44 +0800

    Greate new user, and alert others

--- server/server.js
+++ server/server.js
@@ -32,6 +32,17 @@ io.on('connection',(socket) => {
         text: 'see you then',
         createdAt: 123123
     }); */
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text:'Welcome to the chat app'
+    });
+
+    socket.broadcast.emit('newMessage',{
+        from: 'Admin',
+        text: 'New User joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage',(message) => {
         console.log('createMessage',message);
         io.emit('newMessage', {
@@ -39,6 +50,11 @@ io.on('connection',(socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         });
+        /* socket.broadcast.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        }); */
     });
 
     socket.on('disconnect', () => {
