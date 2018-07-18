0d8188784705ac41ace791866adea36837e51f44
lijin <coolagain2006@gmail.com>
2018-07-18 10:15:23 +0800

    Emit newMessage on createMessge

--- server/server.js
+++ server/server.js
@@ -34,6 +34,11 @@ io.on('connection',(socket) => {
     });
     socket.on('createMessage',(message) => {
         console.log('createMessage',message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
