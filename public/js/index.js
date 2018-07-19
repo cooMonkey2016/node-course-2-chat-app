63c5d0d1dd216c12fba33007e9cc612925f27ebb
lijin <coolagain2006@gmail.com>
2018-07-19 10:35:48 +0800

    Format timestamps using moment js

--- public/js/index.js
+++ public/js/index.js
@@ -19,9 +19,10 @@ socket.on('disconnect',function () {
     console.log('New Email',email);
 }); */
 socket.on('newMessage',function (message) {
-    console.log('newMessage',message);
+    // console.log('newMessage',message);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = $('<li></li>');
-    li.text('from:' + message.from + ' : ' + message.text);
+    li.text('from:' + message.from +' '+ formattedTime +  ' : ' + message.text);
     $('#messages').append(li);
 });
 
@@ -34,9 +35,10 @@ socket.emit('createMessage', {
 });
 
 socket.on('newLocationMessage',function(message) {
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = $('<li></li>');
     var a = $('<a target="_blank">My currrent Location</a>');
-    li.text(message.from + ':');
+    li.text(message.from + ':' + ' ' + formattedTime);
     a.attr('href',message.url);
     li.append(a);
     $('#messages').append(li);
